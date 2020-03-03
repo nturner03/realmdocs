f50f5a4519330acbd02c913dc7cfb093ffd845ca
nturner03 <44912011+nturner03@users.noreply.github.com>
2020-03-03 11:44:09 -0500

    links

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <div id="navbar" class="navbar">
-      <a class="navlink active" href="index.html">Home</a>
+      <a class="navlink active" href="/">Home</a>
       <a class="navlink" href="ultra.html">Ultra</a>
       <a class="navlink" href="valkaria.html">Valkaria</a>
       <a class="navlink" href="#">Alkachwitz</a>
